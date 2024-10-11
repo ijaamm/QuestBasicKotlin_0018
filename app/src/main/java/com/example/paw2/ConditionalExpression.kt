10ed523bc35701987daedc751983d7ccec42203b
ijaamm <ijammusyaf@gmail.com>
2024-10-12 00:00:59 +0700

    Initial commit

---
+++ app/src/main/java/com/example/paw2/ConditionalExpression.kt
@@ -0,0 +1,69 @@
+package com.example.paw2
+
+// Conditional Expression
+// Conditional Expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditional Expression menggunakan if-else, when, dan try-catch
+fun ConditionalStatement() {
+    println("===Conditional Expression===")
+
+    //if-else
+    val number = 10
+    if (number > 0) {
+        println("Positif Number")
+    } else {
+        println("Negative Number")
+    }
+
+    //when
+    val day = 1
+    when (day) {
+        1 -> println("Sunday")
+        2 -> println("Monday")
+        3 -> println("Tuesday")
+        4 -> println("Wednesday")
+        5 -> println("Thursday")
+        6 -> println("Friday")
+        7 -> println("Saturday")
+        else -> println("Invalid Day")
+    }
+
+    // try catch
+    val value = "10"
+    try {
+        val result = value.toInt()
+        println("Result $result")
+    } catch (e: NumberFormatException) {
+        println("Invalid Number")
+    }
+}
+
+fun Perulangan(){
+    println()
+    println("===Perulangan===")
+
+    for (i in 1..5){
+        println("Perulangan ke-$i")
+    }
+
+    val cars = listOf("BMW", "Toyota", "Honda", "Audi")
+    for (car in cars){
+        println("Car type: $car")
+    }
+
+    var x = 1
+    while (x <= 5){
+     println("Perulangan ke-$x")
+     x++
+    }
+
+    var y = 1
+    do {
+        println("Perulangan ke-$y")
+        y++
+    }while (y <= 5)
+}
+
+fun main(){
+    ConditionalStatement()
+    Perulangan()
+}
---
+++ app/src/main/java/com/example/paw2/TipeData.kt
@@ -0,0 +1,42 @@
+package com.example.paw2
+
+fun main(){
+    //byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    //short
+    val shortValue: Short = 20
+    println("Short: $shortValue")
+
+    //int
+    val intValue: Int = 30
+    println("int: $intValue")
+
+    //long
+    val longValue: Long = 40
+    println("long: $longValue")
+
+    //float
+    val floatValue: Float = 50.6f
+    println("Float: $floatValue")
+
+    //double
+    val doubleValue: Double = 60.6
+    println("Double: $doubleValue")
+
+    //char
+    val charValue: Char = 'A'
+    println("Char: $charValue")
+
+    //boolean
+    val booleanValue: Boolean = true
+    println("Boolean: $booleanValue")
+
+    //string
+    var stringValue: String = "Hello World"
+    println("string: $stringValue")
+
+    stringValue = "Hello Kotlin"
+    println("Stirng: $stringValue")
+}
